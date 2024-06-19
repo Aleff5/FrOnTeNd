3327c753af2b5a4a9905fbcc2b5e0d862f7707bd
aleff campos <aleff.mc@sempreceub.com>
2024-06-19 14:23:51 -0300

    mudanças

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import ImageUploader from './components/ImageUploader.vue';
+import Navbar from './components/Navbar.vue';
+</script>
+
+<template>
+  <div id="app">
+    <Navbar />
+    <div class="content">
+      <ImageUploader />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  text-align: center;
+}
+
+.content {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+h1 {
+  margin-top: 0;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,61 @@
+<template>
+    <nav class="navbar">
+      <div class="navbar-brand">
+        <a href="#" class="navbar-item">BrandName</a>
+      </div>
+      <div class="navbar-menu">
+        <div class="navbar-start">
+          <a href="#" class="navbar-item">Home</a>
+          <a href="#" class="navbar-item">About</a>
+        </div>
+        <div class="navbar-end">
+          <a href="#" class="navbar-item">Login</a>
+          <a href="#" class="navbar-item">Sign Up</a>
+        </div>
+      </div>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Navbar',
+  };
+  </script>
+  
+  <style scoped>
+  .navbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+    background-color: #333;
+    color: #fff;
+  }
+  
+  .navbar-brand .navbar-item {
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: #fff;
+    text-decoration: none;
+  }
+  
+  .navbar-menu {
+    display: flex;
+    align-items: center;
+  }
+  
+  .navbar-start, .navbar-end {
+    display: flex;
+  }
+  
+  .navbar-item {
+    margin: 0 0.5rem;
+    color: #fff;
+    text-decoration: none;
+  }
+  
+  .navbar-item:hover {
+    text-decoration: underline;
+  }
+  </style>
+  
